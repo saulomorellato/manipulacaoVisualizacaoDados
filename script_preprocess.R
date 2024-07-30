6eb2b1e6812a7b532224a0ca0c42ba1cf20712aa
Saulo Almeida Morellato <saulo.morellato>
2024-07-30 07:25:46 -0300

    update

--- script_preprocess.R
+++ script_preprocess.R
@@ -9,14 +9,37 @@ library(janitor)
 
 dados_originais<- read.delim("dass.csv")
 
-dados<- clean_names(dados_originais) %>% 
-  select(!contains(c("0i", "1i", "2i", "3i", "4i", "5i", "6i", "7i", "8i", "9i",
-                     "0e", "1e", "2e", "3e", "4e", "5e", "6e", "7e", "8e", "9e"))) %>% 
+
+
+### PRE PROCESSAMENTO ###
+
+# LIMPANDO OS NOMES DA VARIAVEIS
+
+dados<- clean_names(dados_originais)
+
+
+# REMOVENDO QXXI E QXXE
+
+dados<- dados %>% 
+  select(!contains(c("0i", "1i", "2i", "3i", "4i",
+                     "5i", "6i", "7i", "8i", "9i",
+                     "0e", "1e", "2e", "3e", "4e",
+                     "5e", "6e", "7e", "8e", "9e"))) 
+
+
+# CALCULANDO OS ESCORES
+
+dados<- dados %>% 
   mutate(dep = q3a + q5a + q10a + q13a + q16a + q17a + q21a + q24a + q26a + q31a + q34a + q37a + q38a + q42a,
          ans = q2a + q4a + q7a + q9a + q15a + q19a + q20a + q23a + q25a + q28a + q30a + q36a + q40a + q41a,
          est = q1a + q6a + q8a + q11a + q12a + q14a + q18a + q22a + q27a + q29a + q32a + q33a + q35a + q39a,
          tot = dep + ans + est) %>% 
-  select(!starts_with("q")) %>% 
+  select(!starts_with("q"))
+
+
+# DICOTOMIZAR VARIAVEIS TIPI
+
+dados<- dados %>% 
   mutate(extrovertido = ifelse(tipi1>5,"sim","não"),
          critico = ifelse(tipi2>5,"sim","não"),
          confiavel = ifelse(tipi3>5,"sim","não"),
@@ -27,8 +50,19 @@ dados<- clean_names(dados_originais) %>%
          desorganizado = ifelse(tipi8>5,"sim","não"),
          calmo = ifelse(tipi9>5,"sim","não"),
          convencional = ifelse(tipi10>5,"sim","não")) %>% 
-  select(!starts_with("tipi")) %>% 
+  select(!starts_with("tipi"))
+
+
+# REMOVENDO RESPOSTAS "NAO-CONFIAVEIS"
+
+dados<- dados %>% 
   mutate(drop=vcl6+vcl9+vcl12) %>% 
-  filter(drop<1) %>% 
+  filter(drop==0) %>% 
   select(!starts_with("vcl")) %>% 
   select(-drop)
+
+
+# CODIFICAR VARIAVEIS SOCIODEMOGRAFICAS
+
+dados<- dados %>% 
+  mutate(education)
