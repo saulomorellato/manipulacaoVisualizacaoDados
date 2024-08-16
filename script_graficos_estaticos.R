a58aeb3a7bcaa0ae51e4ac8f546b9e6e07e35bdf
Saulo Almeida Morellato <saulo.morellato>
2024-08-16 18:48:52 -0300

    updating

---
+++ script_graficos_estaticos.R
@@ -0,0 +1,260 @@
+### PACOTES ###
+
+library(gtsummary)
+
+
+
+### TABELA ###
+
+
+# idade, genero, estado civil
+
+dados %>% 
+  select(idade, genero, estado_civil) %>% 
+  tbl_summary()
+
+
+# reordenando estado civil, variavel em negrito
+
+dados %>% 
+  select(idade, genero, estado_civil) %>% 
+  mutate(estado_civil=fct_relevel(estado_civil,
+                                  "Solteiro",
+                                  "Casado",
+                                  "Outro")) %>% 
+  tbl_summary() %>% 
+  bold_labels() %>% show_header_names()
+
+
+
+# modificando cabecalho
+
+tbl<- dados %>% 
+  select(idade, genero, estado_civil) %>% 
+  mutate(estado_civil=fct_relevel(estado_civil,
+                                  "Solteiro",
+                                  "Casado",
+                                  "Outro")) %>% 
+  tbl_summary() %>% 
+  bold_labels()
+
+
+tbl %>% show_header_names()
+
+tbl<- tbl %>%
+  modify_header(label="**Variável**",
+                stat_0="**N = 34,334**")
+
+tbl
+
+
+# modificando o rodape
+
+tbl<- tbl %>%
+  modify_footnote(all_stat_cols() ~ "mediana (Q1, Q3); n (%)")
+
+tbl
+
+
+
+### GRÁFICOS ###
+
+
+### idade x estado civil x genero
+
+dados %>% 
+  ggplot(aes(x=estado_civil,y=idade)) +
+  geom_boxplot()
+
+
+# removendo valores extremos e adicionando genero
+
+dados %>% 
+  filter(idade<100) %>% 
+  ggplot(aes(x=estado_civil,y=idade,fill=genero)) +
+  geom_boxplot()
+
+
+# reordenar a variavel estado_civil
+
+dados %>% 
+  filter(idade<100) %>% 
+  mutate(estado_civil=fct_relevel(estado_civil,
+                                  "Solteiro",
+                                  "Casado",
+                                  "Outro"),
+         genero=fct_relevel(genero,
+                            "Feminino",
+                            "Masculino",
+                            "Outro")) %>% 
+  ggplot(aes(x=estado_civil,y=idade,fill=genero)) +
+  geom_boxplot()
+
+
+# alterar titulo, legenda e tema
+
+dados %>% 
+  filter(idade<100) %>% 
+  mutate(estado_civil=fct_relevel(estado_civil,
+                                  "Solteiro",
+                                  "Casado",
+                                  "Outro"),
+         genero=fct_relevel(genero,
+                            "Feminino",
+                            "Masculino",
+                            "Outro")) %>% 
+  ggplot(aes(x=estado_civil,y=idade,fill=genero)) +
+  geom_boxplot() +
+  theme_bw() +
+  labs(x="Estado Civíl",
+       y="Idade",
+       fill="Gênero",
+       title="Inserir Título") +
+  theme(legend.position="right")    # "none", "left", "right", "bottom", "top", "inside"
+  
+
+
+### cor/raça
+
+dados %>% 
+  ggplot(aes(x=cor_raca)) + 
+  geom_bar()
+
+
+# colocar na horizontal
+
+dados %>% 
+  ggplot(aes(y=cor_raca)) + 
+  geom_bar()
+
+
+# modo alternativo de escrever
+
+dados %>% 
+  group_by(cor_raca) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=cor_raca)) + 
+  geom_bar(stat="identity")
+
+
+# reordenar conforme frequencia
+
+dados %>% 
+  group_by(cor_raca) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=reorder(cor_raca,freq))) + 
+  geom_bar(stat="identity")
+
+
+# adicionar cor
+
+dados %>% 
+  group_by(cor_raca) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=reorder(cor_raca,freq))) + 
+  geom_bar(stat="identity",fill="blue")
+
+
+# cor conforme classe
+
+dados %>% 
+  group_by(cor_raca) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=reorder(cor_raca,freq),fill=cor_raca)) + 
+  geom_bar(stat="identity")
+
+
+# cor conforme frequencia
+
+dados %>% 
+  group_by(cor_raca) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=reorder(cor_raca,freq),fill=freq)) + 
+  geom_bar(stat="identity")
+
+
+# cor conforme frequencia + tema + remover legenda
+
+dados %>% 
+  group_by(cor_raca) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=reorder(cor_raca,freq),fill=-freq)) + 
+  geom_bar(stat="identity") +
+  theme_bw() +
+  labs(x="Frequência",
+       y="",
+       #fill="",
+       title="Cor/Raça") +
+  theme(legend.position="none")    # "none", "left", "right", "bottom", "top", "inside"
+
+
+
+### Religiao (Pizza?)
+
+dados %>% 
+  group_by(religiao) %>% 
+  summarise(freq = n()) %>%
+  ggplot(aes(x=freq,y=reorder(religiao,freq),fill=-freq)) + 
+  geom_bar(stat="identity") +
+  theme_bw() +
+  labs(x="Frequência",
+       y="",
+       #fill="",
+       title="Religião") +
+  theme(legend.position="none")    # "none", "left", "right", "bottom", "top", "inside"
+
+
+
+### Escores
+
+dados %>% 
+  ggplot(aes(x=tot)) +
+  geom_histogram()
+
+
+# reduzindo o numero de "bins"
+
+dados %>% 
+  ggplot(aes(x=tot)) +
+  geom_histogram(bins=15)
+
+
+# escolhendo cores + tema
+
+dados %>% 
+  ggplot(aes(x=tot)) +
+  geom_histogram(bins=15, colour="black", fill="white") +
+  theme_bw()
+
+
+# adicionando distribuicao empirica
+
+dados %>% 
+  ggplot(aes(x=tot)) +
+  geom_histogram(aes(y=after_stat(density)),
+                 bins=15,
+                 colour="black",
+                 fill="white") +
+  geom_density(alpha=.2, fill="blue") + 
+  theme_bw()
+
+
+# mudando labels
+
+dados %>% 
+  ggplot(aes(x=tot)) +
+  geom_histogram(aes(y=after_stat(density)),
+                 bins=15,
+                 colour="black",
+                 fill="white") +
+  geom_density(alpha=.2, fill="blue") + 
+  theme_bw() +
+  labs(x="Escore",
+       y="Densidade",
+       #fill="",
+       title="")
+
+
+
+
+
--- script_preprocess.R
+++ script_preprocess.R
@@ -13,9 +13,11 @@ dados_originais<- read.delim("dass.csv")
 
 ### PRE PROCESSAMENTO ###
 
-# LIMPANDO OS NOMES DA VARIAVEIS
+# LIMPANDO OS NOMES DA VARIAVEIS / REMOVENDO NA's
 
-dados<- clean_names(dados_originais)
+dados<- dados_originais %>% 
+  clean_names() %>% 
+  drop_na()
 
 
 # REMOVENDO QXXI E QXXE
@@ -65,4 +67,201 @@ dados<- dados %>%
 # CODIFICAR VARIAVEIS SOCIODEMOGRAFICAS
 
 dados<- dados %>% 
-  mutate(education)
+  mutate(educacao=ifelse(education==1,
+                          "Fundamental",
+                          ifelse(education==2,
+                                 "Médio",
+                                 ifelse(education==3,
+                                        "Superior",
+                                        "Pós-Graduação"))),
+         moradia_infancia=ifelse(urban==1,"Rural",
+                                 ifelse(urban==2,
+                                        "Suburbio",
+                                        "Urbano")),
+         genero=ifelse(gender==1,
+                       "Masculino",
+                       ifelse(gender==2,
+                              "Feminino",
+                              "Outro")),
+         idioma=ifelse(engnat==1,"Inglês","Outro"),
+         idade=age,
+         lateralidade=ifelse(hand==1,
+                             "Destro",
+                             ifelse(hand==2,
+                                    "Canhoto",
+                                    "Ambidestro")),
+         religiao=ifelse(religion==1,
+                         "Agnótico",
+                         ifelse(religion==2,
+                                "Ateu",
+                                ifelse(religion==3,
+                                       "Budista",
+                                       ifelse(religion==4,
+                                              "Católico",
+                                              ifelse(religion==5,
+                                                     "Mórmon",
+                                                     ifelse(religion==6,
+                                                            "Protestante",
+                                                            ifelse(religion==7,
+                                                                   "Critão/Outro",
+                                                                   ifelse(religion==8,
+                                                                          "Hindu",
+                                                                          ifelse(religion==9,
+                                                                                 "Judeu",
+                                                                                 ifelse(religion==10,
+                                                                                        "Muçulmano",
+                                                                                        ifelse(religion==11,
+                                                                                               "Sikh",
+                                                                                               "Outro"))))))))))),
+         orientacao=ifelse(orientation==1,
+                           "Heterossexual",
+                           ifelse(orientation==2,
+                                  "Bissexual",
+                                  ifelse(orientation==3,
+                                         "Homossexual",
+                                         ifelse(orientation==4,
+                                                "Assexual",
+                                                "Outro")))),
+         cor_raca=ifelse(race==10,
+                         "Asiático",
+                         ifelse(race==20,
+                                "Árabe",
+                                ifelse(race==30,
+                                       "Negro",
+                                       ifelse(race==40,
+                                              "Indígena Australiano",
+                                              ifelse(race==50,
+                                                     "Nativo Americano",
+                                                     ifelse(race==60,
+                                                            "Branco",
+                                                            "Outro")))))),
+         estado_civil=ifelse(married==1,
+                             "Solteiro",
+                             ifelse(married==2,
+                                    "Casado",
+                                    "Outro")),
+         irmaos=familysize-1,
+         pais=country,
+         dispositivo=ifelse(screensize==1,"Celular","Computador"),
+         fonte=ifelse(source==1,
+                      "Página DASS",
+                      ifelse(source==2,
+                             "Google",
+                             "Outro"))) %>% 
+  select(-c(education,
+            urban,
+            gender,
+            engnat,
+            age,
+            hand,
+            religion,
+            orientation,
+            race,
+            voted,
+            married,
+            familysize,
+            major,
+            country,
+            screensize,
+            uniquenetworklocation,
+            source))
+
+
+
+
+dados<- dados %>% 
+  mutate(educacao=recode(as.character(education),
+                          "1"="Fundamental",
+                          "2"="Médio",
+                          "3"="Superior",
+                          "4"="Pós-Graduação"),
+         moradia_infancia=recode(as.character(urban),
+                                 "1"="Rural",
+                                 "2"="Suburbio",
+                                 "3"="Urbano"),
+         genero=recode(as.character(gender),
+                       "1"="Masculino",
+                       "2"="Feminino",
+                       "3"="Outro"),
+         idioma=recode(as.character(engnat),
+                       "1"="Inglês",
+                       "2"="Outro"),
+         idade=age,
+         lateralidade=recode(as.character(hand),
+                             "1"="Destro",
+                             "2"="Canhoto",
+                             "3"="Ambidestro"),
+         religiao=recode(as.character(religion),
+                         "1"="Agnóstico",
+                         "2"="Ateu",
+                         "3"="Budista",
+                         "4"="Católico",
+                         "5"="Mórmon",
+                         "6"="Protestante",
+                         "7"="Critão/Outro",
+                         "8"="Hindu",
+                         "9"="Judeu",
+                         "10"="Muçulmano",
+                         "11"="Sikh",
+                         "12"="Outro"),
+         orientacao=recode(as.character(orientation),
+                           "1"="Heterossexual",
+                           "2"="Bissexual",
+                           "3"="Homossexual",
+                           "4"="Assexual",
+                           "5"="Outro"),
+         cor_raca=recode(as.character(race),
+                         "10"="Asiático",
+                         "20"="Árabe",
+                         "30"="Negro",
+                         "40"="Indígena Australiano",
+                         "50"="Nativo Americano",
+                         "60"="Branco",
+                         "70"="Outro"),
+         estado_civil=recode(as.character(married),
+                             "1"="Solteiro",
+                             "2"="Casado",
+                             "3"="Outro"),
+         irmaos=familysize-1,
+         pais=country,
+         dispositivo=ifelse(screensize==1,"Celular","Computador"),
+         fonte=recode(as.character(source),
+                      "1"="Página DASS",
+                      "2"="Google",
+                      "0"="Outro")) %>% 
+  select(-c(education,
+            urban,
+            gender,
+            engnat,
+            age,
+            hand,
+            religion,
+            orientation,
+            race,
+            voted,
+            married,
+            familysize,
+            major,
+            country,
+            screensize,
+            uniquenetworklocation,
+            source))
+
+
+
+# REMOCAO DE VALORES INVALIDOS
+
+dados<- dados %>% 
+  filter(genero!=0,
+         estado_civil!=0)
+
+
+
+# TRANSFORMAR NAO NUMERICAS EM FATOR
+
+dados<- dados %>%
+  unclass() %>%
+  as.data.frame(stringsAsFactors=TRUE)
+
+
+
